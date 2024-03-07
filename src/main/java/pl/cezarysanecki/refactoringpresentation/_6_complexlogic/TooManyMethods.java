c4124b814f362c71c0466d21dfef25932e2463ac
Cezary Sanecki <saneckicezary@gmail.com>
2024-03-07 17:11:35 +0100

    6 case inlined methods

--- src/main/java/pl/cezarysanecki/refactoringpresentation/_6_complexlogic/TooManyMethods.java
+++ src/main/java/pl/cezarysanecki/refactoringpresentation/_6_complexlogic/TooManyMethods.java
@@ -14,71 +14,40 @@ class TooManyMethods {
     }
 
     void call() {
-        printMinimize();
-        printSpace();
-        printCognitive();
-        printSpace();
-        printLoad();
-        printSpace();
-        printFor();
-        printSpace();
-        printOthers();
-    }
-
-    private static void printOthers() {
+        System.out.print("M");
+        System.out.print("i");
+        System.out.print("n");
+        System.out.print("i");
+        System.out.print("m");
+        System.out.print("i");
+        System.out.print("z");
+        System.out.print("e");
+        System.out.print(" ");
+        System.out.print("c");
+        System.out.print("o");
+        System.out.print("g");
+        System.out.print("n");
+        System.out.print("i");
+        System.out.print("t");
+        System.out.print("i");
+        System.out.print("v");
+        System.out.print("e");
+        System.out.print(" ");
+        System.out.print("l");
+        System.out.print("o");
+        System.out.print("a");
+        System.out.print("d");
+        System.out.print(" ");
+        System.out.print("f");
+        System.out.print("o");
+        System.out.print("r");
+        System.out.print(" ");
         System.out.print("o");
         System.out.print("t");
         System.out.print("h");
         System.out.print("e");
         System.out.print("r");
         System.out.print("s");
-    }
-
-    private static void printLoad() {
-        System.out.print("l");
-        System.out.print("o");
-        System.out.print("a");
-        System.out.print("d");
-    }
-
-    private static void printSpace() {
-        System.out.print(" ");
-    }
-
-    private static void printCognitive() {
-        System.out.print("c");
-        System.out.print("o");
-        System.out.print("g");
-        printNi();
-        System.out.print("t");
-        printI();
-        System.out.print("v");
-        System.out.print("e");
-    }
-
-    private static void printI() {
-        System.out.print("i");
-    }
-
-    private static void printFor() {
-        System.out.print("f");
-        System.out.print("o");
-        System.out.print("r");
-    }
-
-    private static void printNi() {
-        System.out.print("n");
-        printI();
-    }
-
-    private static void printMinimize() {
-        System.out.print("M");
-        printI();
-        printNi();
-        System.out.print("m");
-        printI();
-        System.out.print("z");
-        System.out.print("e");
     }
 
 }
