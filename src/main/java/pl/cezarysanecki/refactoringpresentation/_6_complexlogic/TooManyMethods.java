2233e1d7a5aaa3517b508f8c545ab92913a2b186
Cezary Sanecki <saneckicezary@gmail.com>
2024-03-07 17:12:11 +0100

    6 case spotted pattern

--- src/main/java/pl/cezarysanecki/refactoringpresentation/_6_complexlogic/TooManyMethods.java
+++ src/main/java/pl/cezarysanecki/refactoringpresentation/_6_complexlogic/TooManyMethods.java
@@ -14,40 +14,7 @@ class TooManyMethods {
     }
 
     void call() {
-        System.out.print("M");
-        System.out.print("i");
-        System.out.print("n");
-        System.out.print("i");
-        System.out.print("m");
-        System.out.print("i");
-        System.out.print("z");
-        System.out.print("e");
-        System.out.print(" ");
-        System.out.print("c");
-        System.out.print("o");
-        System.out.print("g");
-        System.out.print("n");
-        System.out.print("i");
-        System.out.print("t");
-        System.out.print("i");
-        System.out.print("v");
-        System.out.print("e");
-        System.out.print(" ");
-        System.out.print("l");
-        System.out.print("o");
-        System.out.print("a");
-        System.out.print("d");
-        System.out.print(" ");
-        System.out.print("f");
-        System.out.print("o");
-        System.out.print("r");
-        System.out.print(" ");
-        System.out.print("o");
-        System.out.print("t");
-        System.out.print("h");
-        System.out.print("e");
-        System.out.print("r");
-        System.out.print("s");
+        System.out.println("Minimize cognitive load for others");
     }
 
 }
