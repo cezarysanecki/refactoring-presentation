2fceeab5b623548c2a0a62b5476d43501326bca4
Cezary Sanecki <saneckicezary@gmail.com>
2024-03-07 15:52:31 +0100

    Initial commit

---
+++ src/main/java/pl/cezarysanecki/refactoringpresentation/RefactoringPresentationApplication.java
@@ -0,0 +1,13 @@
+package pl.cezarysanecki.refactoringpresentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringPresentationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefactoringPresentationApplication.class, args);
+    }
+
+}
